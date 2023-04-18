29eb3e2380e3d5e3b788e69062f7365cbb12cc20
Michal99Zajac <michal.99.zajac@gmail.com>
2023-04-18 20:42:35 +0200

    remove junks

--- program.c
+++ program.c
@@ -575,7 +575,10 @@ void sauvola_threshold_with_integral_image(unsigned char **grayscale,
       sum_squares = D_sq - B_sq - C_sq + A_sq;
 
       // Compute the mean and standard deviation for the local region
-      count = (right - left + 1) * (bottom - top + 1);
+      count =
+          (right - left + 1) *
+          (bottom - top +
+           1); // can't be changed, the squar area could be different each loop
       mean = sum / (double)count;
       stdev = sqrt((sum_squares / (double)count) - (mean * mean));
 
@@ -672,11 +675,11 @@ void pgm_sauvola_flow_with_integral_image(const char *input_file_name,
   unsigned long long ***integral_image =
       alloc_integral_image(num_rows, num_cols);
 
-  // Calculate integral image
-  compute_integral_image(grayscale, integral_image, num_cols, num_rows);
-
   // start timing
   start_time = clock();
+
+  // Calculate integral image
+  compute_integral_image(grayscale, integral_image, num_cols, num_rows);
 
   // Sauvola threshold
   sauvola_threshold_with_integral_image(grayscale, integral_image, output,
@@ -696,165 +699,6 @@ void pgm_sauvola_flow_with_integral_image(const char *input_file_name,
       0)
     exit(1);
 
-  free(grayscale[0]);
-  free(grayscale);
-  free(output[0]);
-  free(output);
-  free(integral_image[0][0]);
-  free(integral_image[0]);
-  free(integral_image);
-}
-
-void ppm_sauvola_flow(const char *input_file_name,
-                      const char *output_file_name) {
-  int num_rows, num_cols;
-  int max_color;
-  int header_length, i, j;
-  clock_t start_time, end_time;
-  double elapsed_time;
-
-  // Read header to get dimensions and max color value
-  if ((header_length = read_ppm_header(input_file_name, &num_rows, &num_cols,
-                                       &max_color)) <= 0)
-    exit(1);
-
-  // Allocate memory for grayscale array
-  unsigned char **grayscale = alloc_2D_unsigned_char(num_rows, num_cols);
-
-  // Allocate memory for RGB arrays
-  unsigned char **red_channel = alloc_2D_unsigned_char(num_rows, num_cols);
-  unsigned char **green_channel = alloc_2D_unsigned_char(num_rows, num_cols);
-  unsigned char **blue_channel = alloc_2D_unsigned_char(num_rows, num_cols);
-
-  // Read RGB data from file
-  if (read_ppm_data(red_channel[0], green_channel[0], blue_channel[0],
-                    input_file_name, header_length, num_rows, num_cols,
-                    max_color) == 0)
-    exit(1);
-
-  // Compute grayscale values from RGB values
-  unsigned char r, g, b, gray_value;
-  for (i = 0; i < num_rows; ++i) {
-    for (j = 0; j < num_cols; ++j) {
-      r = red_channel[i][j];
-      g = green_channel[i][j];
-      b = blue_channel[i][j];
-      gray_value = (unsigned char)((0.299 * r) + (0.587 * g) + (0.114 * b));
-      grayscale[i][j] = gray_value;
-    }
-  }
-
-  // Allocate memory for output array
-  unsigned char **output = alloc_2D_unsigned_char(num_rows, num_cols);
-
-  // start timing
-  start_time = clock();
-
-  // Sauvola threshold
-  sauvola_threshold(grayscale, output, num_cols, num_rows, 0.5, 13, 255);
-
-  // end timing
-  end_time = clock();
-
-  // calculate elapsed time in milliseconds
-  elapsed_time = ((double)(end_time - start_time) / CLOCKS_PER_SEC) * 1000.0;
-
-  // print elapsed time
-  printf("Sauvola Elapsed time: %f ms\n", elapsed_time);
-
-  // write pgm file
-  if (write_pgm_image(output_file_name, output[0], num_rows, num_cols, 255) ==
-      0)
-    exit(1);
-
-  free(red_channel[0]);
-  free(red_channel);
-  free(green_channel[0]);
-  free(green_channel);
-  free(blue_channel[0]);
-  free(blue_channel);
-  free(grayscale[0]);
-  free(grayscale);
-  free(output[0]);
-  free(output);
-}
-
-void ppm_sauvola_flow_with_integral_image(const char *input_file_name,
-                                          const char *output_file_name) {
-  int num_rows, num_cols;
-  int max_color;
-  int header_length, i, j;
-  clock_t start_time, end_time;
-  double elapsed_time;
-
-  // Read header to get dimensions and max color value
-  if ((header_length = read_ppm_header(input_file_name, &num_rows, &num_cols,
-                                       &max_color)) <= 0)
-    exit(1);
-
-  // Allocate memory for grayscale array
-  unsigned char **grayscale = alloc_2D_unsigned_char(num_rows, num_cols);
-
-  // Allocate memory for RGB arrays
-  unsigned char **red_channel = alloc_2D_unsigned_char(num_rows, num_cols);
-  unsigned char **green_channel = alloc_2D_unsigned_char(num_rows, num_cols);
-  unsigned char **blue_channel = alloc_2D_unsigned_char(num_rows, num_cols);
-
-  // Read RGB data from file
-  if (read_ppm_data(red_channel[0], green_channel[0], blue_channel[0],
-                    input_file_name, header_length, num_rows, num_cols,
-                    max_color) == 0)
-    exit(1);
-
-  // Compute grayscale values from RGB values
-  unsigned char r, g, b, gray_value;
-  for (i = 0; i < num_rows; ++i) {
-    for (j = 0; j < num_cols; ++j) {
-      r = red_channel[i][j];
-      g = green_channel[i][j];
-      b = blue_channel[i][j];
-      gray_value = (unsigned char)((0.299 * r) + (0.587 * g) + (0.114 * b));
-      grayscale[i][j] = gray_value;
-    }
-  }
-
-  // Allocate memory for output array
-  unsigned char **output = alloc_2D_unsigned_char(num_rows, num_cols);
-
-  // Allocate memory for integral image 3D array
-  unsigned long long ***integral_image =
-      alloc_integral_image(num_rows, num_cols);
-
-  // Calculate integral image
-  compute_integral_image(grayscale, integral_image, num_cols, num_rows);
-
-  // start timing
-  start_time = clock();
-
-  // Sauvola threshold
-  sauvola_threshold_with_integral_image(grayscale, integral_image, output,
-                                        num_cols, num_rows, 0.5, 13, 255);
-
-  // end timing
-  end_time = clock();
-
-  // calculate elapsed time in milliseconds
-  elapsed_time = ((double)(end_time - start_time) / CLOCKS_PER_SEC) * 1000.0;
-
-  // print elapsed time
-  printf("Sauvola with Integral Image Elapsed time: %f ms\n", elapsed_time);
-
-  // write pgm file
-  if (write_pgm_image(output_file_name, output[0], num_rows, num_cols, 255) ==
-      0)
-    exit(1);
-
-  free(red_channel[0]);
-  free(red_channel);
-  free(green_channel[0]);
-  free(green_channel);
-  free(blue_channel[0]);
-  free(blue_channel);
   free(grayscale[0]);
   free(grayscale);
   free(output[0]);
@@ -874,13 +718,6 @@ int main(int argc, char **argv) {
 
   pgm_sauvola_flow_with_integral_image("./media/016_lanczos.pgm",
                                        "./media/016_lanczos_converted_ii.pgm");
-
-  printf("\n\n");
-
-  ppm_sauvola_flow("./media/maly.ppm", "./media/maly_converted.pgm");
-
-  ppm_sauvola_flow_with_integral_image("./media/maly.ppm",
-                                       "./media/maly_converted_ii.pgm");
 
   return 0;
 }
